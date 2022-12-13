0fbcdf70ae38fe7f6564cfad8436cfdd7ee463dc
Gabriel Almeida <116915862+gaalmeida-iblue@users.noreply.github.com>
2022-12-13 11:56:36 -0300

    errorHandler implementation, auth middleware for basic strategy

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,5 +1,36 @@
 import passport from "passport";
+import { Request, Response, NextFunction } from 'express'
+import { BasicStrategy } from 'passport-http'
+import { User } from '../models/User'
 
-passport.use()
+const notAuthorizedJson = {
+    status: 401,
+    message: "Não autorizado"
+}
+
+passport.use(new BasicStrategy( async (email, password, done) => {
+    if(email && password) {
+        const user = await User.findOne({
+            where: { email, password }
+         });
+         if(user) {
+            return done(null, user);
+         }
+    } else {
+        return done(notAuthorizedJson, false);
+    }
+}))
+
+export const privateRoute = (req: Request, res: Response, next: NextFunction) => {
+    const authFunction = passport.authenticate('basic', (err, user) => {
+        if(user) {
+            next()
+        } else {
+            next(notAuthorizedJson)
+        }
+    })
+
+    authFunction(req, res, next)
+}
 
 export default passport
